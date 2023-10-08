a874e3a993255767a2b18c1ffccaae83ed7e8963
Filip Skaug <filis@stud.ntnu.no>
2023-10-08 18:04:17 +0200

    Horserace finish
    Co-authored-by: olejacobmellgren <olejacobmellgren@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import './App.css';
+import { CSSProperties } from 'react';
 import ReactTyped from "react-typed";
 import Horse from './components/Horse';
-import { useEffect, useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import OkonomiTor from './assets/OkonomiTor.png';
 import Bergen from './assets/Bergen.png';
 import Aas from './assets/Aas.png';
@@ -10,26 +10,104 @@ import Spidermann from './assets/Spidermann.png';
 function App() {
   const [isVisible, setIsVisible] = useState(false);
 
-  function handleStart() {
+  function handleVisible() {
     setIsVisible(!isVisible);
   }
 
+  // interface moveProps {
+  //   horse: HorseProps;
+  //   xValue: number;
+  // }
 
+  interface HorseProps {
+    imgPath: string;
+    style: CSSProperties;
+    id: string;
+  }
+
+  const [horses, setHorses] = useState<{ [id: string]: HorseProps }>({
+    "tor": {
+      imgPath: OkonomiTor,
+      style: { left: 'calc(1 * (100vw - 250px) / 5)' },
+      id: 'tor'
+    },
+    "bergen": {
+      imgPath: Bergen,
+      style: { left: 'calc(2 * (100vw - 250px) / 5)' },
+      id: 'bergen'
+    },
+    "aas": {
+      imgPath: Aas,
+      style: { left: 'calc(3 * (100vw - 250px) / 5)' },
+      id: 'aas'
+    },
+    "spider": {
+      imgPath: Spidermann,
+      style: { left: 'calc(4 * (100vw - 250px) / 5)' },
+      id: 'spider'
+    }
+  });
+  const horseRef = useRef(horses)
+  horseRef.current = horses
+
+
+  function handleStart() {
+    handleVisible();
+    //set horses to start position
+    let currentX = [0, 0, 0, 0];
+
+    const startHorses = {
+      "tor": { ...horses.tor, style: { ...horses.tor.style, left: currentX[0], top: '0px' } },
+      "bergen": { ...horses.bergen, style: { ...horses.bergen.style, left: currentX[1], top: '200px' } },
+      "aas": { ...horses.aas, style: { ...horses.aas.style, left: currentX[2], top: '400px' } },
+      "spider": { ...horses.spider, style: { ...horses.spider.style, left: currentX[3], top: '600px' } }
+    }
+    setHorses(startHorses)
+    horseRef.current = startHorses;
+
+
+
+    function moveHorse() {
+      const randomHorseIndex = Math.floor(Math.random() * 4);
+      const randomHorse = Object.keys(horses)[randomHorseIndex];
+      currentX[randomHorseIndex] += 150;
+      setTimeout(() => {
+        const test = { ...horseRef.current }
+        test[randomHorse] = { ...test[randomHorse], style: { ...test[randomHorse].style, left: (currentX[randomHorseIndex].toString() + 'px') } }
+        setHorses(test)
+        horseRef.current = test
+        if (currentX[randomHorseIndex] <= 1650) { // not finished
+          moveHorse()
+        } 
+        else {
+          return;
+        }
+      }, 900); // Adjust the delay as needed (in milliseconds)
+
+    }
+    moveHorse();
+  }
 
   return (
     <>
-      <Horse imgPath={OkonomiTor} selftop={10}/>
-      <Horse imgPath={Bergen} selftop={20}/>
-      <Horse imgPath={Aas} selftop={30}/>
-      <Horse imgPath={Spidermann} selftop={40}/>
-      {isVisible && (<h1><ReactTyped strings={["GAME INITIATED STARTING IN:", "5" ,"4" ,"3", "2", "1", "GO!"]} typeSpeed={50} showCursor={false} onComplete={handleStart}/></h1>) }
+      <div className="horse-container">
+        {Object.values(horses).map((horse) => (<Horse id={horse.id} imgPath={horse.imgPath} style={horse.style} />))}
+      </div>
+      {isVisible && (
+        <>
+          <h1>
+            <ReactTyped
+              strings={["GAME INITIATED STARTING IN:", "5", "4", "3", "2", "1", "GO!"]}
+              typeSpeed={40}
+              onComplete={handleStart}
+            />
+          </h1>
+        </>)}
       <div className="ButtonWrapper">
-        <button onClick={handleStart}>
+        <button onClick={handleVisible}>
           Start race
         </button>
       </div>
-
-     
     </>
   )
 }
--- src/components/Horse.tsx
+++ src/components/Horse.tsx
@@ -1,24 +1,17 @@
-
-
+import { CSSProperties, useEffect, useState } from "react";
 interface HorseProps{
   imgPath: string;
-  selftop: number;
+  style: CSSProperties;
+  id: string;
 }
 
-const Horse: React.FC<HorseProps> = ({ imgPath, selftop }) => {
-  const style = {
-    // top: selftop,
-    // position: absolute,
-    // top: 10vh,
-    // width: 10vw,
-    // height: 10vw,
-  }
-  // funciton moveHorse() {
-  //   position += 10;
-  // }
+const Horse: React.FC<HorseProps> = ({ imgPath, style, id }) => {
   return (
-    <img className="Horse" src={imgPath}/>
+    <>
+      <img style={style} id={id} className="horse" src={imgPath} />
+    </>
   )
 }
 
 export default Horse;
+
