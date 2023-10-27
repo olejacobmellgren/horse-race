a67d098aef7ed1efde86a982713a88056e9d23bd
Filip Skaug <filis@stud.ntnu.no>
2023-10-27 16:07:38 +0200

    add style

--- src/components/ButtonPage.tsx
+++ src/components/ButtonPage.tsx
@@ -1,19 +1,10 @@
-import confetti from "https://esm.run/canvas-confetti@1";
-
 const ButtonPage = ({horse}: {horse: string}) => {
-  function onClick() {
-    confetti({
-      particleCount: 150,
-      spread: 60,
-      startVelocity: 30,
-    });
-  }
+
 
   return (
     <div className="button-body">
       <button 
         className="go-button"
-        onClick={ onClick }
       >
         GO {horse}!!
       </button>
