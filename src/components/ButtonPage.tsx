15e59a959bd36d220fcd8cf80fd2e7a698440a20
Filip Skaug <filis@stud.ntnu.no>
2023-10-27 15:54:46 +0200

    style button

--- src/components/ButtonPage.tsx
+++ src/components/ButtonPage.tsx
@@ -1,7 +1,22 @@
+import confetti from "https://esm.run/canvas-confetti@1";
+
 const ButtonPage = ({horse}: {horse: string}) => {
+  function onClick() {
+    confetti({
+      particleCount: 150,
+      spread: 60,
+      startVelocity: 30,
+    });
+  }
+
   return (
-    <div className="body">
-      <button className="go-button">GO {horse}!!</button>
+    <div className="button-body">
+      <button 
+        className="go-button"
+        onClick={ onClick }
+      >
+        GO {horse}!!
+      </button>
     </div>
   );
 };
